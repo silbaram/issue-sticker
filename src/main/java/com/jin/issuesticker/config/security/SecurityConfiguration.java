75f32136d89f4d013ab0aea9bd40b86beddbc6c8
psj <silbaram79@naver.com>
2020-05-14 23:24:55 +0900

    소스 정리

--- src/main/java/com/jin/issuesticker/config/security/SecurityConfiguration.java
+++ src/main/java/com/jin/issuesticker/config/security/SecurityConfiguration.java
@@ -4,17 +4,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.WebFilterChainServerAuthenticationSuccessHandler;
 import org.springframework.security.web.server.authentication.logout.DelegatingServerLogoutHandler;
 import org.springframework.security.web.server.authentication.logout.HeaderWriterServerLogoutHandler;
 import org.springframework.security.web.server.authentication.logout.SecurityContextServerLogoutHandler;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutHandler;
 import org.springframework.security.web.server.header.ClearSiteDataServerHttpHeadersWriter;
+import reactor.core.publisher.Mono;
 
 import static org.springframework.security.web.server.header.ClearSiteDataServerHttpHeadersWriter.Directive.CACHE;
 import static org.springframework.security.web.server.header.ClearSiteDataServerHttpHeadersWriter.Directive.COOKIES;
@@ -41,19 +42,20 @@ public class SecurityConfiguration {
         http
         .csrf(csrf -> csrf.disable())
         .authorizeExchange()
+//        .pathMatchers(HttpMethod.DELETE).denyAll()
         .pathMatchers(resources).permitAll()
         .pathMatchers("/security/**").permitAll()
         .anyExchange().authenticated()
-        .and()
-        .redirectToHttps(redirect -> redirect.httpsRedirectWhen(e -> e.getRequest().getHeaders().containsKey("X-Forwarded-Proto"))
-        .and()
-        .httpBasic()
+//        .and()
+//        .redirectToHttps(redirect -> redirect.httpsRedirectWhen(e -> e.getRequest().getHeaders().containsKey("X-Forwarded-Proto"))
         .and()
         .formLogin()
         .loginPage("/security/login")
+        .authenticationFailureHandler((webFilterExchange, e) -> Mono.error(e))
+        .authenticationSuccessHandler(new WebFilterChainServerAuthenticationSuccessHandler())
         .and()
         .logout()
-        .logoutHandler(logoutHandler));
+        .logoutHandler(logoutHandler);
 
         return http.build();
     }
