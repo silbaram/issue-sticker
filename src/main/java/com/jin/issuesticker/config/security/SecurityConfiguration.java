682c37cec3e1720b74652cec5a86d1b391a23a8e
psj <psj@i-screamedu.co.kr>
2020-05-20 17:45:53 +0900

    로그인시 토큰을 생성하기 위한 부분까지 완성

--- src/main/java/com/jin/issuesticker/config/security/SecurityConfiguration.java
+++ src/main/java/com/jin/issuesticker/config/security/SecurityConfiguration.java
@@ -6,12 +6,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authentication.WebFilterChainServerAuthenticationSuccessHandler;
-import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 import reactor.core.publisher.Mono;
 
 
@@ -37,6 +36,7 @@ public class SecurityConfiguration {
 
         http
         .csrf(csrf -> csrf.disable())
+        .formLogin(formLoginSpec -> formLoginSpec.disable())
         .logout(logoutSpec -> logoutSpec.disable())
         .httpBasic(httpBasicSpec -> httpBasicSpec.disable());
 
@@ -49,24 +49,17 @@ public class SecurityConfiguration {
         .anyExchange().authenticated()
 
         .and()
-        .formLogin()
-        .loginPage("/security/login")
-        .authenticationFailureHandler((webFilterExchange, e) -> Mono.error(e))
-        .authenticationSuccessHandler(new WebFilterChainServerAuthenticationSuccessHandler())
-
-        .and()
         // 로그인 실패, 권한이 없을시 특정페이지로 redirect 가 아닌 실패 응답 (REST API를 사용하기 위함)
         .exceptionHandling()
-        .authenticationEntryPoint((serverWebExchange, e) -> Mono.error(e))
-        .accessDeniedHandler((serverWebExchange, e) -> Mono.error(e))
-        //.authenticationEntryPoint((serverWebExchange, e) -> Mono.fromRunnable((() -> serverWebExchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED))))
-        //.accessDeniedHandler((serverWebExchange, e) -> Mono.fromRunnable((() -> serverWebExchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN))))
+        .authenticationEntryPoint((serverWebExchange, e) -> Mono.fromRunnable((() -> serverWebExchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED))))
+        .accessDeniedHandler((serverWebExchange, e) -> Mono.fromRunnable((() -> serverWebExchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN))))
 
 //        .and()
 //        .securityContextRepository(NoOpServerSecurityContextRepository.getInstance());
         //.and()
         //.redirectToHttps(redirect -> redirect.httpsRedirectWhen(e -> e.getRequest().getHeaders().containsKey("X-Forwarded-Proto"));
         ;
+
         return http.build();
     }
 }
